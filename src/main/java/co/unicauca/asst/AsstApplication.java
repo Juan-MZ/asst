54888400d4592cd613a936d68d433ccd93661c6d
Juan-MZ <juanjosz@unicauca.edu.co>
2024-03-06 09:15:29 -0500

    project creation

---
+++ src/main/java/co/unicauca/asst/AsstApplication.java
@@ -0,0 +1,13 @@
+package co.unicauca.asst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/unicauca/asst/AsstApplicationTests.java
@@ -0,0 +1,13 @@
+package co.unicauca.asst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
